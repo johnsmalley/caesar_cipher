fb9c5696805677c8f5cb73e2e747a5fc7d205bd6
johnsmalley <john.m.smalley@gmail.com>
2016-01-10 11:23:15 -0500

    Problem Solved

---
+++ caesar_cipher.rb
@@ -0,0 +1,35 @@
+=begin
+This program takes in a string and shift the output by a number of charters
+to keep the message a secret. This is based on the Caesar Cipher project
+at The Odin Project
+http://www.theodinproject.com/ruby-programming/building-blocks	
+=end
+
+puts "Type in text that will be disguised"
+input_message = gets.chomp
+
+puts "Enter a number for the amount of charters your message should get" +
+	 " shifted by"
+shift = gets.chomp
+
+count = 0
+input_message.each_char do |char|
+	new_position = char.ord + shift.to_i
+
+	if char.ord.between?(65, 90)
+		while new_position > 90
+			new_position -= 26
+		end
+		input_message[count] = new_position.chr
+	elsif char.ord.between?(97, 122)
+		while new_position > 122
+			new_position -= 26
+		end
+		input_message[count] = new_position.chr
+	end
+	count += 1
+end
+
+puts "Your new disguised message is shown below"
+puts input_message
+
